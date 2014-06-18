e5fb47847cca46b2d03cfea8a5db3e2bd0306f0e
Sasha Cooper <jinksy@gmail.com>
2014-06-18 09:29:14 +0100

    trying to make args work - tests currently fail

--- lib/inject.rb
+++ lib/inject.rb
@@ -2,7 +2,7 @@ require 'debugger'
 module Enumerable 
 
 	def my_inject(accumulator = self.first, &block)
-		return accumulator if self.length == 1
+		return block.call(accumulator, self[0]) if self.length == 1
 		block.call(accumulator, self.drop(1).my_inject(&block))
 	end
 
