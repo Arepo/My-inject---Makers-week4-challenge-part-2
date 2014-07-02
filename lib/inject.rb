fc52f96ff4cf0af0dc8138a63c1ad4c0ecfedfd3
Sasha Cooper <jinksy@gmail.com>
2014-07-02 18:59:35 +0100

    mini version of inject working

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,13 +1,12 @@
-require 'debugger'
-module Enumerable 
 
-	def my_inject(accumulator = self.first, &block)
-		return block.call(accumulator, self[0]) if self.length == 1
-		block.call(accumulator, self.drop(1).my_inject(&block))
+module Enumerable 
+	
+	def my_inject(accumulator = nil, &block)
+		return accumulator if self.length == 0
+		return self.first if !accumulator && self.length == 1
+		return yield(accumulator, self.first) if self.length == 1
+		return self.drop(1).my_inject(yield(accumulator, self.first), &block) if accumulator
+		return self.drop(2).my_inject(yield(self[0], self[1]), &block) 
 	end
 
 end
-
-	# def define_caller
-	# 	self.class
-	# end
